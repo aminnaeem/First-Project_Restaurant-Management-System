e7f9fb4f96486bcf68b7150b34da9f76a73e9a44
aminnaeem <amin.naeem1233@gmail.com>
2023-07-23 23:20:16 +0500

    Updating Print Bill Code

--- Restaurant_Management_System/Counter/Options/Print_Bill.dart
+++ Restaurant_Management_System/Counter/Options/Print_Bill.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import '../isCounter.dart';
 import 'Create_Order.dart';
 
 DateTime now = DateTime.now();
@@ -11,8 +10,8 @@ String formatDate(DateTime dateTime) {
 String formatTime(DateTime dateTime) {
   return "${dateTime.hour}:${dateTime.minute}:${dateTime.second}";
 }
-String formattedDate = formatDate(now);
-String formattedTime = formatTime(now);
+String Date = formatDate(now);
+String Time = formatTime(now);
 
 
 isPrintBill () {
@@ -20,31 +19,34 @@ isPrintBill () {
   print('');
   print('Bill');
   print('');
-  print('Date: $formattedDate, Time: $formattedTime');
+  print('Date: $Date, Time: $Time');
   print('');
+  bool bill = false;
 
-  Order.forEach((item) { 
-    String itemName = item['Item'];
-    int itemQuantity = item['Quantity'];
-    int itemPrice = item['Price'];
-    int totalItemPrice = item['Total'];
-    print('$itemQuantity $itemName  Price: $itemPrice  Total: $totalItemPrice');
-  });
-  num totalAmount = 0;
-  for (var item in Order) {
-    totalAmount += item['Total'];
-  }
-  print('-----------------------------');
-  print('Total Amount: Rs. $totalAmount/-');
-  print('Cash Received:');
-  String cashR = stdin.readLineSync()!;
-  int cashReceived = int.parse(cashR);
-  print('Cash Returned: Rs. ${cashReceived-totalAmount}/-');
-  print('-----------------------------');
-  print('Thanks for Visiting Spicy Tonight');
-  print('');
-  print('Press any key to Main Menu');
+  while (bill == false) {
+    Order.forEach((item) { 
+      String itemName = item['Item'];
+      int itemQuantity = item['Quantity'];
+      int itemPrice = item['Price'];
+      int totalItemPrice = item['Total'];
+      print('$itemQuantity $itemName  Price: $itemPrice  Total: $totalItemPrice');
+    });
+    num totalAmount = 0;
+    for (var item in Order) {
+      totalAmount += item['Total'];
+    }
+    print('-----------------------------');
+    print('Total Amount: Rs. $totalAmount/-');
+    print('Cash Received:');
+     String cashR = stdin.readLineSync()!;
+    int cashReceived = int.parse(cashR);
+    print('Cash Returned: Rs. ${cashReceived-totalAmount}/-');
+    print('-----------------------------');
+    print('Thanks for Visiting Spicy Tonight');
+    print('');
+    print('Press any key to Main Menu');
   
-  String? rtn = stdin.readLineSync(); 
-  isCounter();
+    String? rtn = stdin.readLineSync(); 
+    bill = true;
+  }
 }
