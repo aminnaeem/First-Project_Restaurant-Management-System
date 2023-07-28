2968ceb992060d5f5df90064bcf050864351330e
aminnaeem <amin.naeem1233@gmail.com>
2023-07-28 18:55:27 +0500

    Updating Remove item code and removing runtime errors

--- Restaurant_Management_System/Counter/Functions/Remove_Item.dart
+++ Restaurant_Management_System/Counter/Functions/Remove_Item.dart
@@ -20,37 +20,13 @@ isRemoveItem () {
   String ri = stdin.readLineSync()!;
   int removeItem = int.parse(ri);
 
-
-  print('${Order[removeItem-1]['Item']} is removed');
-
-  switch (removeItem) {
-    case 1 :
-            Order.removeAt(0);
-      break;
-    case 2 :
-            Order.removeAt(1);
-      break;
-    case 3 :
-            Order.removeAt(2);
-      break;
-    case 4:
-            Order.removeAt(3);
-      break;
-    case 5 :
-            Order.removeAt(4);
-      break;
-    case 6 :
-            Order.removeAt(5);
-      break;
-    case 7 :
-            Order.removeAt(6);
-      break;
-    case 8 :
-            Order.removeAt(7);
-      break;
-    default:
-            isCounter();
-  }  
+  if (removeItem > 0 && removeItem < Order.length) {
+    print('${Order[removeItem-1]['Item']} is removed from your order');
+    Order.removeAt(removeItem-1);
+  }
+  else{
+    return isCounter();
+  }
 }
 
 void main (){
