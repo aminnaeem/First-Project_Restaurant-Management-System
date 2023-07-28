2d0c43eed47cdb15f429780dfcfd22ba328ce3c2
aminnaeem <amin.naeem1233@gmail.com>
2023-07-28 18:54:41 +0500

    Updating Print bill code and removing runtime errors

--- Restaurant_Management_System/Counter/Functions/Print_Bill.dart
+++ Restaurant_Management_System/Counter/Functions/Print_Bill.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import '../isCounter.dart';
 import 'Create_Order.dart';
 
 DateTime now = DateTime.now();
@@ -15,42 +14,36 @@ String Date = formatDate(now);
 String Time = formatTime(now);
 
 
-isPrintBill () {
+bool isPrintBill () {
   print('Spicy Tonight');
   print('');
   print('Bill');
   print('');
   print('Date: $Date, Time: $Time');
   print('');
-  bool bill = false;
 
-  while (bill == false) {
-    Order.forEach((item) { 
-      String itemName = item['Item'];
-      int itemQuantity = item['Quantity'];
-      int itemPrice = item['Price'];
-      int totalItemPrice = item['Total'];
-      print('$itemQuantity $itemName  Price: $itemPrice  Total: $totalItemPrice');
-    });
-    num totalAmount = 0;
-    for (var item in Order) {
-      totalAmount += item['Total'];
-    }
-    print('-----------------------------');
-    print('Total Amount: Rs. $totalAmount/-');
-    print('Cash Received:');
-     String cashR = stdin.readLineSync()!;
-    int cashReceived = int.parse(cashR);
-    print('Cash Returned: Rs. ${cashReceived-totalAmount}/-');
-    print('-----------------------------');
-    print('Thanks for Visiting Spicy Tonight');
-    print('');
-    print('Press any key to Main Menu');
-    bill = true;
+  Order.forEach((item) { 
+    String itemName = item['Item'];
+    int itemQuantity = item['Quantity'];
+    int itemPrice = item['Price'];
+    int totalItemPrice = item['Total'];
+    print('$itemQuantity $itemName  Price: $itemPrice  Total: $totalItemPrice');
+  });
+  num totalAmount = 0;
+  for (var item in Order) {
+    totalAmount += item['Total'];
   }
+  print('-----------------------------');
+  print('Total Amount: Rs. $totalAmount/-');
+  print('Cash Received:');
+  String cashR = stdin.readLineSync()!;
+  int cashReceived = int.parse(cashR);
+  print('Cash Returned: Rs. ${cashReceived-totalAmount}/-');
+  print('-----------------------------');
+  print('Thanks for Visiting Spicy Tonight');
+  print('');
+  print('Press any key to Main Menu');
 
   String? rtn = stdin.readLineSync(); 
-  if (rtn == null || rtn != null) {
-    isCounter();
-  }
+  return true;
 }
